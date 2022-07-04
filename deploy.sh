e32a25f723dc65e8744980013442ec260b49b4a0
yanghong <15034009501@163.com>
2022-07-04 14:53:14 +0800

    更新

--- deploy.sh
+++ deploy.sh
@@ -8,6 +8,10 @@ set -e
 npm run docs:build
 git config --global --unset http.proxy
 git config --global --unset https.proxy
+#git init
+#git add -A
+#git commit -m '更新-提交'
+#git push -f  https://github.com/keyboarder-yang/blog master
 # 删除远程分支
 git push origin --delete gh-pages
 git subtree push --prefix docs/.vitepress/dist origin gh-pages
