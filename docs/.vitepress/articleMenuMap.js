fcdbc0104d1c43caefea8bc74cc46abd9af5031c
yanghong <15034009501@163.com>
2022-07-09 17:20:42 +0800

    this指向问题更新

--- docs/.vitepress/articleMenuMap.js
+++ docs/.vitepress/articleMenuMap.js
@@ -5,8 +5,8 @@
 const files = {
     'JavaScript': {
         sidebarName: '',
-        label: ['数组遍历', '类', '闭包', '数据类型', '解构', '事件循环', '原型', '递归', '正则', 'this', '时间计数', '树遍历'],
-        name: ['arrayTraversal', 'Class', 'Closure', 'DataType', 'deconstruct', 'EventLoop', 'Prototype', 'Recursion', 'RegExp', 'This', 'TimeCount', 'TreeTraversal'],
+        label: ['数组遍历', '类', '闭包', '数据类型', '解构', '事件循环', '原型', '递归', '正则', 'this', 'new关键字', '时间计数', '树遍历'],
+        name: ['arrayTraversal', 'Class', 'Closure', 'DataType', 'deconstruct', 'EventLoop', 'Prototype', 'Recursion', 'RegExp', 'This', 'New', 'TimeCount', 'TreeTraversal'],
     },
     'Vue': {
         label: ['render函数', '插槽', '数据持久化存储'],
